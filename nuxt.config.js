d24a5b245651ff1c17fbf2976f6a63032af8a85e
Stephen Kastona <stephenkastona@gmail.com>
2020-06-29 21:11:57 +0100

    final please

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
 
-  target: 'server',
+  target: 'static',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
