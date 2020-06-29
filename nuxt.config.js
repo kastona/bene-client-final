88167777d0a21f571b6e5b443f32db5ce8f1a98e
Stephen Kastona <stephenkastona@gmail.com>
2020-06-29 22:19:42 +0100

    eya

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 
 export default {
+
+  server: {
+    port: process.env.PORT || 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   /*
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
